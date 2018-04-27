1f87005c44d615d9dd20e3679964190c2dca7a93
haos <haoshuai6720@163.com>
2018-04-27 17:51:12 +0800

    郝帅提交

--- src/main/java/com/baizhi/TestGit.java
+++ src/main/java/com/baizhi/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("this is first option~~");
         System.out.println("this is xiao di");
+        System.out.println("this is haoshuai~~~~");
     }
 }
