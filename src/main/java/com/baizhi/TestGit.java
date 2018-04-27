310ffdd30889b4554916bf816cf58e25d02bc4da
haos <haoshuai6720@163.com>
2018-04-27 17:49:55 +0800

    xiao di 第一次提交

--- src/main/java/com/baizhi/TestGit.java
+++ src/main/java/com/baizhi/TestGit.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("this is first option~~");
+        System.out.println("this is xiao di");
     }
 }
